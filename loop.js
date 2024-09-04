8d27dc8731d7a253dff60446eaebba326d21a6f1
MORSHED-ALAM-s <malamidb90@gmail.com>
2024-09-04 23:19:03 +0600

    for loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+// for(let i = 1; i <= 5; i++){
+//     console.log('My Name is Morshed Alam', i)
+// }
+
+const fruits = ['Mango', 'Jack-fruit', 'Guava']
+
+// for(let i = 0; i <=fruits.length; i++)
+//     console.log(fruits[i])
+for(let i =fruits.length-1; i>=0; i--){
+    console.log(fruits[i])
+}
