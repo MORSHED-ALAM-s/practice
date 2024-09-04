7bab1dc6e8ae2e024eaf59320bad658e7a35bd0c
MORSHED-ALAM-s <malamidb90@gmail.com>
2024-09-04 22:17:35 +0600

    arry practices

---
+++ argument.js
@@ -0,0 +1,37 @@
+
+// // // //Access the golden rod color value in output.
+// // // // const colors = {
+// // // //     red: "#ff0000",
+// // // //     green: "#00ff00",
+// // // //     blue: "#0000ff",
+// // // //     "golden rod": '#daa520'
+// // // // };
+
+// // // const numbers = [34, 35, 47, 28];
+// // // const x = numbers.pop()
+// // // console.log(numbers)
+// // // numbers.push(80)
+// // // console.log(numbers)
+// // // console.log(x)
+// // // numbers.pop()
+// // // console.log(numbers)
+// // // console.log(x)
+
+ const fruits = ['Mango', 'Jack-fruit', 'Guava']
+// // const flowers = ['Rose', 'Bali', 'Night-rose', 'Merry']
+// // const number =[1, 58, 85]
+// // const ffplus = fruits.concat(flowers,number);
+// // //console.log(ffplus);
+// // // console.log(ffplus[8])
+
+// //  //const find = flowers.indexOf([3])
+// // //  const find = flowers.includes('Merry')
+// // //  console.log(find)
+// // const total = ffplus.join(' ')
+// // console.log(total)
+
+//const string ="Mango, Apple, Nut, Guava"
+// const convert = string.split(',')
+// console.log(convert)
+fruits.unshift("Nut")
+console.log(fruits)
