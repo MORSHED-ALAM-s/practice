329e239d9326d1c1e58cfe33756b1d8d75f23519
MORSHED-ALAM-s <malamidb90@gmail.com>
2024-09-08 21:56:39 +0600

    function

---
+++ conceptual.js
@@ -0,0 +1,30 @@
+// function showName( ){
+//     console.log("morshed")
+// }
+// showName();
+
+// function showName(name,age,address){
+//     console.log(name,age,address)
+// }
+// showName("Morshe Alam", 35, "Rangpur");
+
+
+// function add(x,y){
+//     const total = x + y;
+//     console.log(total);
+// }
+// add(5,9);
+// function giveName(){
+//     const x ="Alo"
+//     return x;
+// }
+// const oname = giveName();
+// console.log(oname)
+
+/*
+Types of function (4)
+1. No Parameters no return;
+2.Parameters with no return
+3.Parameter with parameters
+4.No parameters but return
+*/
--- loop.js
+++ loop.js
@@ -6,6 +6,28 @@ const fruits = ['Mango', 'Jack-fruit', 'Guava']
 
 // for(let i = 0; i <=fruits.length; i++)
 //     console.log(fruits[i])
-for(let i =fruits.length-1; i>=0; i--){
-    console.log(fruits[i])
+// for(let i =fruits.length-1; i>=0; i--){
+//     console.log(fruits[i])
+// }
+// let i = 0;
+
+// while(i < fruits.length){
+//     console.log(fruits[i]);
+//     i++;
+// }
+// let i = fruits.length-1;
+// while(i >=0){
+//     console.log(fruits[i])
+//     i--;
+// }
+
+// for(let item of fruits){
+//     console.log(item)
+// }
+// for(let item of fruits.reverse()){
+//         console.log(item)
+//      }
+function getMenu(){
+        console.log("BUrger, Pizza, Pasta")
 }
+getMenu()
